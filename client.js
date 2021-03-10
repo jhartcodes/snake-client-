de2f4eea79366646bd8f26588ad61962a2532066
Joel <joel.d.hart1@gmail.com>
2021-03-10 21:33:14 +0000

    added name to snake

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require('net');
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('connect', function () {
+    console.log("connection established!")
+    conn.write('Name: JH')
+  })
+
+  conn.on('data', function (data) {
+    console.log(data);
+  });
+
+
+  return conn;
+}
+
+module.exports = connect;
